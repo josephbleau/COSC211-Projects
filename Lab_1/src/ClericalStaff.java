caf8b02466e84e1826a28a133210fd4bdaed50a9
Joseph Bleau <jbleau@gmail.com>
2012-10-11 13:58:39 -0400

    Added lab 1

---
+++ Lab_1/src/ClericalStaff.java
@@ -0,0 +1,11 @@
+public class ClericalStaff extends TechnicalStaff{
+	ClericalStaff(int id, String name, Date date, double salary, int rank)
+	{
+		super(id, name, date, salary, rank);
+	}
+	
+	public void print()
+	{
+		super.print();
+	}
+}
---
+++ Lab_1/src/Executive.java
@@ -0,0 +1,11 @@
+public class Executive extends SalariedEmployee{
+	Executive(int id, String name, Date date, double salary)
+	{
+		super(id, name, date, salary, 6);
+	}
+	
+	public void print()
+	{
+		super.print();
+	}
+}
